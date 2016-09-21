cb8d3933f9b3f25fbac0e5aec6f8d45965cef97a
Yung-Luen Lan <yllan@me.com>
2016-09-21 14:00:17 +0800

    Initial Commit

---
+++ phenidone/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  phenidone
+//
+//  Created by Yung-Luen Lan on 21/09/2016.
+//  Copyright © 2016 Hypo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
